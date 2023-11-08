e0a81bff61f5041676d1b9f853e5d2cd408fcd44
Asmaa Saleh <asmaatareq1999@gmail.com>
2023-11-08 21:11:30 +0200

    change the name of the file and add more features

--- lib/screen/search_page.dart
+++ lib/screen/search_page.dart
@@ -1,19 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:spectrum_speak/widgets/card_specialist.dart';
 import 'package:spectrum_speak/widgets/card_center.dart';
 import 'package:spectrum_speak/widgets/card_item.dart';
 
 import '../const.dart';
+import '../units/build_radio_button.dart';
 
 class Centers extends StatefulWidget {
-  const Centers({super.key});
+  const Centers({Key? key}) : super(key: key);
 
   @override
   State<Centers> createState() => _CentersState();
 }
 
 class _CentersState extends State<Centers> {
+  Search selectedSearch = Search.center;
+
+  void onSearchTypeChanged(Search newValue) {
+    setState(() {
+      selectedSearch = newValue;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    List<Color> cardColors = [kBlue, kRed, kGreen, kYellow,kBlue, kRed, kGreen, kYellow,kBlue, kRed, kGreen, kYellow];
+    List<Widget> cards = [];
+
+    for (Color color in cardColors) {
+      if (selectedSearch == Search.specialist) {
+        cards.add(CardSpecialist(cardColor: color));
+      } else if (selectedSearch == Search.center) {
+        cards.add(CenterCard(cardColor: color));
+      }
+    }
     return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
@@ -26,7 +46,7 @@ class _CentersState extends State<Centers> {
                 child: Padding(
                   padding: const EdgeInsets.only(top: 20, bottom: 40),
                   child: Text(
-                    "Rehabilitation centres",
+                    "Search",
                     style: TextStyle(
                       fontSize: 25.0,
                       fontWeight: FontWeight.bold,
@@ -34,6 +54,10 @@ class _CentersState extends State<Centers> {
                     ),
                   ),
                 ),
+              ),
+              RadioButtonSearch(
+                selectedSearch: selectedSearch,
+                onSearchTypeChanged: onSearchTypeChanged,
               ),
               TextField(
                 decoration: InputDecoration(
@@ -69,21 +93,11 @@ class _CentersState extends State<Centers> {
                   ),
                 ),
               ),
-              // const CenterCard(),
-              // const CenterCard(),
-              const Center(
+              Center(
                 child: Wrap(
                   spacing: 50.0, // Horizontal spacing between items
                   runSpacing: 30.0, // Vertical spacing between lines
-                  children: <Widget>[
-                    CenterCard(cardColor: kBlue),
-                    CenterCard(cardColor: kRed),
-                    CenterCard(cardColor: kGreen),
-                    CenterCard(cardColor: kYellow),
-                    CenterCard(cardColor: kBlue),
-                    CenterCard(cardColor: kRed),
-                    CenterCard(cardColor: kGreen),
-                  ],
+                  children: cards, // Use the list of cards here
                 ),
               ),
             ],
