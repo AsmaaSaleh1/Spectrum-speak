9baaf8fd6337b8942f5f1c358f1da2dc6685e3e1
Asmaa Saleh <asmaatareq1999@gmail.com>
2023-11-11 17:09:31 +0200

    shadow teacher(card+clipper)

---
+++ lib/units/custom_clipper_shadow_teacher.dart
@@ -0,0 +1,57 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class CustomClipperShadowTeacher extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    double widthSize = size.width;
+    double heightSize = size.height;
+
+    double radius = widthSize / 6; // Change to width for top and bottom circles
+    double radius2 = heightSize / 6; // Change to height for left and right circles
+
+    // Draw the square
+    path.moveTo(0, radius);
+    path.arcToPoint(
+      Offset(radius, 0),
+      radius: Radius.circular(radius),
+      largeArc: false,
+      clockwise: false,
+    );
+
+    path.lineTo(widthSize - radius, 0);
+    path.arcToPoint(
+      Offset(widthSize, radius),
+      radius: Radius.circular(radius),
+      largeArc: false,
+      clockwise: false,
+    );
+
+    path.lineTo(widthSize, heightSize - radius2);
+    path.arcToPoint(
+      Offset(widthSize - radius2, heightSize),
+      radius: Radius.circular(radius2),
+      largeArc: false,
+      clockwise: false,
+    );
+
+    path.lineTo(radius2, heightSize);
+    path.arcToPoint(
+      Offset(0, heightSize - radius2),
+      radius: Radius.circular(radius2),
+      largeArc: false,
+      clockwise: false,
+    );
+
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return true;
+  }
+}
