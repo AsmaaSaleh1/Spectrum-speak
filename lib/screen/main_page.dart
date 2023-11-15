bd3028d404bcd054800dcacdb8e369f5655089fa
Asmaa Saleh <asmaatareq1999@gmail.com>
2023-11-15 15:12:40 +0200

    new empty page

---
+++ lib/screen/main_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:spectrum_speak/widgets/top_bar.dart';
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TopBar(
+      body: Text("MAIN"),
+    );
+  }
+}
