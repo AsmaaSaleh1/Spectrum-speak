5e2bfbed0c67bd2226d94d485aee22beed761e94
Asmaa Saleh <asmaatareq1999@gmail.com>
2023-11-16 20:29:10 +0200

    change the title

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Profile',
+        title: 'Spectrum Speak',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           appBarTheme: AppBarTheme(
