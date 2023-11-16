1d2d2e081b8b9ccf0309496a420dbfb47c5b0e8d
Asmaa Saleh <asmaatareq1999@gmail.com>
2023-11-16 22:15:01 +0200

    add more details to the topBar

--- lib/widgets/top_bar.dart
+++ lib/widgets/top_bar.dart
@@ -1,6 +1,9 @@
+import 'package:circular_profile_avatar/circular_profile_avatar.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:spectrum_speak/const.dart';
+import 'package:spectrum_speak/screen/main_page.dart';
 import 'package:spectrum_speak/screen/parent_profile.dart';
 import 'package:spectrum_speak/screen/search_page.dart';
 
@@ -14,19 +17,77 @@ class TopBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
     return Scaffold(
+      key: scaffoldKey,
       appBar: AppBar(
-        title: const Center(
-          child: Text(
-            "SPECTRUM SPEAK",
-            textAlign: TextAlign.center,
-            style: TextStyle(
+        leading: IconButton(
+          icon: const Icon(
+            CupertinoIcons.list_bullet, // Change this icon to the one you prefer
+            color: kPrimary,
+            size: 30,
+          ),
+          onPressed: () {
+            // Handle drawer opening
+            scaffoldKey.currentState?.openDrawer();
+          },
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              // Handle the message button press
+              print('Message button pressed');
+            },
+            icon: const Icon(
+              CupertinoIcons.envelope_open,
               color: kPrimary,
+              size: 30,
             ),
           ),
-        ),
+          IconButton(
+            onPressed: () {
+              // Handle the calendar button press
+              print('Calendar button pressed');
+            },
+            icon: const Icon(
+              CupertinoIcons.calendar,
+              color: kPrimary,
+              size: 30,
+            ),
+          ),
+          IconButton(
+            onPressed: () {
+              // Handle the notification button press
+              print('Notification button pressed');
+            },
+            icon: const Icon(
+              CupertinoIcons.bell,
+              color: kPrimary,
+              size: 30,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: CircularProfileAvatar(
+              '',
+              borderWidth: 1.0,
+              borderColor: kPrimary.withOpacity(0.5),
+              radius: 20.0,
+              backgroundColor: kPrimary,
+              onTap: (){
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ParentProfile()),
+                );
+              },
+              child: Image.asset('images/prof.png'),
+            ),
+          )
+        ],
       ),
       drawer: Drawer(
+        shadowColor: kDarkerColor,
+        backgroundColor: kPrimary,
         child: ListView(
           children: [
             UserAccountsDrawerHeader(
@@ -46,13 +107,19 @@ class TopBar extends StatelessWidget {
               ),
               currentAccountPicture: CircleAvatar(
                 backgroundColor: kPrimary,
-                child: ClipOval(
-                  child: Image.asset(
-                    'images/prof.png',
-                    width: 90,
-                    height: 90,
-                    fit: BoxFit.cover,
-                  ),
+                child: CircularProfileAvatar(
+                  '',
+                  borderWidth: 1.0,
+                  borderColor: kPrimary.withOpacity(0.5),
+                  radius: 90.0,
+                  backgroundColor: kPrimary,
+                  onTap: (){
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const ParentProfile()),
+                    );
+                  },
+                  child: Image.asset('images/prof.png'),
                 ),
               ),
               currentAccountPictureSize: const Size.square(75),
@@ -66,12 +133,12 @@ class TopBar extends StatelessWidget {
             ),
             ListTile(
               leading: Icon(
-                FontAwesomeIcons.user,
+                FontAwesomeIcons.house,
                 color: kDarkerColor,
                 size: 22,
               ),
               title: const Text(
-                "Profile",
+                "Home",
                 style: TextStyle(
                   fontWeight: FontWeight.w700,
                   fontSize: 17,
@@ -79,7 +146,7 @@ class TopBar extends StatelessWidget {
               ),
               onTap: () => Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => const ParentProfile()),
+                MaterialPageRoute(builder: (context) => const MainPage()),
               ),
             ),
             ListTile(
