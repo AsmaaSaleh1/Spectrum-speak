4dd9ba1049779b34dd843f03f6a0dd4f38bf87c4
Asmaa Saleh <asmaatareq1999@gmail.com>
2023-11-30 01:04:09 +0200

    add function to signUp (specialist,child,teacher) and check unique email

--- lib/rest/rest_api.dart
+++ lib/rest/rest_api.dart
@@ -26,10 +26,11 @@ Future userSignUp(String email, String userName, String phone, String password,
   var decodedData = jsonDecode(response.body);
   return decodedData;
 }
-//TODO: not tested until the shared key work
-Future specialistSignUp(String userId,double price, String selectedSpecialistCategory)async{
-  final response =
-  await http.post(Uri.parse('${Utils.baseUrl}/signUp/specialist/:user_id'), headers: {
+
+Future specialistSignUp(
+    String userId, double price, String selectedSpecialistCategory) async {
+  final response = await http
+      .post(Uri.parse('${Utils.baseUrl}/signUp/specialist/$userId'), headers: {
     "Accept": "application/json"
   }, body: {
     'Price': price.toString(),
@@ -37,4 +38,54 @@ Future specialistSignUp(String userId,double price, String selectedSpecialistCat
   });
   var decodedData = jsonDecode(response.body);
   return decodedData;
+}
+
+Future shadowTeacherSignUp(String userId, String salary, String birthDate,
+    String availability, String qualification, String gender) async {
+  final response = await http.post(
+      Uri.parse('${Utils.baseUrl}/signUp/shadowTeacher/$userId'),
+      headers: {
+        "Accept": "application/json"
+      },
+      body: {
+        'Salary': salary,
+        'BirthDate': birthDate,
+        'Availability': availability,
+        'AcademicQualification': qualification,
+        'Gender': gender,
+      });
+  var decodedData = jsonDecode(response.body);
+  return decodedData;
+}
+
+Future childrenSignUp(String userId, String name, String birthDate,
+    String gender, String degreeOfAutism) async {
+  final response =
+      await http.post(Uri.parse('${Utils.baseUrl}/signUp/children'), headers: {
+    "Accept": "application/json"
+  }, body: {
+    'UserId': userId,
+    'Name': name,
+    'BirthDate': birthDate,
+    'Gender': gender,
+    'DegreeOfAutism': degreeOfAutism,
+  });
+  var decodedData = jsonDecode(response.body);
+  return decodedData;
+}
+Future<bool> isEmailAlreadyExists(String email) async {
+  final response=
+      await http.post(Uri.parse('${Utils.baseUrl}/checkEmail'),headers: {
+  "Accept": "application/json"
+  }, body: {
+    'Email':email,
+  },
+  );
+  var decodedData = jsonDecode(response.body);
+
+  if (response.statusCode == 200) {
+    return decodedData['exists'];
+  }else{
+    return false;
+  }
 }
