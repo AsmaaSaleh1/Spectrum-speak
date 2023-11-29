703c15a9166447d70213ff4a105b68775e3efcff
Asmaa Saleh <asmaatareq1999@gmail.com>
2023-11-30 01:01:42 +0200

    add clean to store function

--- lib/rest/auth_manager.dart
+++ lib/rest/auth_manager.dart
@@ -3,6 +3,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 class AuthManager {
   static Future<void> storeUserData(String userId, String userEmail) async {
     final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.clear();
     prefs.setString('userID', userId);
     prefs.setString('userEmail', userEmail);
   }
