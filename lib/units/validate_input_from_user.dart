ec151d7c1a3a6af0a2883745425a0a03dd878d75
Asmaa Saleh <asmaatareq1999@gmail.com>
2023-11-30 01:05:10 +0200

    new file validation user input

---
+++ lib/units/validate_input_from_user.dart
@@ -0,0 +1,28 @@
+// Email validation function
+bool isValidEmail(String email) {
+  final emailRegex = RegExp(r'^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$');
+  return email.isNotEmpty && emailRegex.hasMatch(email);
+}
+// Phone validation function
+bool isValidPhoneNumber(String phoneNumber) {
+  return int.tryParse(phoneNumber) != null;
+}
+// Password validation function
+bool isPasswordValid(String password) {
+  // Password must contain at least one lowercase letter, one uppercase letter,
+  // one number, and have a minimum length of 8 characters.
+  final passwordRegex =
+  RegExp(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$');
+  return passwordRegex.hasMatch(password);
+}
+// decimal(7,2) validation function
+bool isDecimal(String input) {
+  final RegExp decimalRegExp = RegExp(r'^\d{1,5}(\.\d{1,2})?$');
+  return decimalRegExp.hasMatch(input);
+}
+//Date [yyyy-mm-dd] validation function
+bool isDate(String input) {
+  final RegExp dateRegExp = RegExp(
+      r'^\d{4}-\d{2}-\d{2}$'); // Matches the 'yyyy-mm-dd' format (e.g., '2023-11-29')
+  return dateRegExp.hasMatch(input);
+}
