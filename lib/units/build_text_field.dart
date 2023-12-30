7dab4973df4b46c05f49279e8c07526be6c51ebd
Asmaa Saleh <asmaatareq1999@gmail.com>
2023-12-30 23:11:29 +0200

    add counter to count the number of chars add maxLines

--- lib/units/build_text_field.dart
+++ lib/units/build_text_field.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:spectrum_speak/constant/const_color.dart';
+
 class CustomTextField extends StatefulWidget {
   final IconData? preIcon;
   final String labelText;
@@ -8,15 +9,21 @@ class CustomTextField extends StatefulWidget {
   final bool isPasswordTextField;
   final TextEditingController controller;
   final bool disable;
-  const CustomTextField(
-      {super.key,
-      this.preIcon,
-      required this.labelText,
-      required this.placeholder,
-      required this.isPasswordTextField,
-      required this.controller,
-        this.disable=false,
-      });
+  final int numOfLine;
+  final Function? onChange;
+  final int maxCharacterCount;
+  const CustomTextField({
+    super.key,
+    this.preIcon,
+    required this.labelText,
+    required this.placeholder,
+    required this.isPasswordTextField,
+    required this.controller,
+    this.disable = false,
+    this.numOfLine = 1,
+    this.onChange,
+    this.maxCharacterCount=1000,
+  });
 
   @override
   State<CustomTextField> createState() => _CustomTextFieldState();
@@ -24,6 +31,7 @@ class CustomTextField extends StatefulWidget {
 
 class _CustomTextFieldState extends State<CustomTextField> {
   bool isObscurePassword = true;
+  int _characterCount = 0;
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -31,27 +39,28 @@ class _CustomTextFieldState extends State<CustomTextField> {
       child: TextField(
         controller: widget.controller,
         obscureText: widget.isPasswordTextField ? isObscurePassword : false,
+        maxLines: widget.numOfLine,
         decoration: InputDecoration(
           filled: widget.disable,
           fillColor: Colors.grey.shade400,
           prefixIcon: widget.preIcon != null
               ? Icon(
-                  widget.preIcon,
-                  color: kDarkerColor,
-                )
+            widget.preIcon,
+            color: kDarkerColor,
+          )
               : null,
           suffixIcon: widget.isPasswordTextField
               ? IconButton(
-                  onPressed: () {
-                    setState(() {
-                      isObscurePassword = !isObscurePassword;
-                    });
-                  },
-                  icon: Icon(
-                    isObscurePassword ? Icons.visibility : Icons.visibility_off,
-                    color: kDarkerColor,
-                  ),
-                )
+            onPressed: () {
+              setState(() {
+                isObscurePassword = !isObscurePassword;
+              });
+            },
+            icon: Icon(
+              isObscurePassword ? Icons.visibility : Icons.visibility_off,
+              color: kDarkerColor,
+            ),
+          )
               : null,
           contentPadding: const EdgeInsets.only(
             bottom: 5,
@@ -89,6 +98,24 @@ class _CustomTextFieldState extends State<CustomTextField> {
           color: kDarkerColor,
         ),
         readOnly: widget.disable,
+        onChanged: (text) {
+          if (widget.onChange != null) {
+            widget.onChange!(text);
+          }
+
+          setState(() {
+            _characterCount = text.length;
+          });
+
+          if (_characterCount > widget.maxCharacterCount) {
+            // Truncate the text to the maximum allowed characters
+            widget.controller.text =
+                widget.controller.text.substring(0, widget.maxCharacterCount);
+            widget.controller.selection = TextSelection.fromPosition(
+              TextPosition(offset: widget.maxCharacterCount),
+            );
+          }
+        },
       ),
     );
   }
