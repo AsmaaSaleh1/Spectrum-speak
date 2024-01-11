365db043c3bae35ea27170ce3fab2a9deb426c46
asmaa <asmaatareq1999@gmail.com>
2024-01-11 16:48:08 +0200

    add a button(add/done) to card choose specialist

--- lib/widgets/card_choose_specialist.dart
+++ lib/widgets/card_choose_specialist.dart
@@ -2,6 +2,7 @@ import 'package:circular_profile_avatar/circular_profile_avatar.dart';
 import 'package:flutter/material.dart';
 import 'package:spectrum_speak/constant/const_color.dart';
 import 'package:spectrum_speak/screen/specialist_profile.dart';
+import 'package:spectrum_speak/units/custom_button.dart';
 
 class CardChooseSpecialist extends StatefulWidget {
   final String userID;
@@ -19,16 +20,26 @@ class CardChooseSpecialist extends StatefulWidget {
 }
 
 class _CardChooseSpecialistState extends State<CardChooseSpecialist> {
+  bool isButtonPressed = false;
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 3,
       shadowColor: kDarkerColor,
       color: kPrimary,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
+      ),
       child: InkWell(
         onTap: () {
-          Navigator.push(context, MaterialPageRoute(builder:(_)=>const SpecialistProfile()));
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (_) => SpecialistProfile(
+                userId: widget.userID,
+              ),
+            ),
+          );
         },
         child: ListTile(
           leading: CircularProfileAvatar(
@@ -44,11 +55,41 @@ class _CardChooseSpecialistState extends State<CardChooseSpecialist> {
           ),
           title: Text(
             widget.userName,
-            style: TextStyle(color: kDarkerColor),
+            style: TextStyle(
+              color: kDarkerColor,
+            ),
           ),
           subtitle: Text(
             widget.specialistCategory,
             maxLines: 1,
+          ),
+          trailing: ElevatedButton.icon(
+            onPressed: () {
+              setState(() {
+                // Toggle the button state
+                isButtonPressed = !isButtonPressed;
+              });
+            },
+            style: ElevatedButton.styleFrom(
+              foregroundColor: kPrimary,
+              backgroundColor: isButtonPressed ? kGreen : kBlue,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(
+                  7,
+                ),
+              ),
+            ),
+            icon: Icon(
+              isButtonPressed ? Icons.done : Icons.add,
+              color: kPrimary,
+            ),
+            label: Text(
+              isButtonPressed ? "Done" : "Add",
+              style: TextStyle(
+                color: kPrimary,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
         ),
       ),
