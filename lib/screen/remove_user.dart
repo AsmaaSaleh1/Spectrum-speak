31855e0372ad6ead60310a713c4d2c4bef153a69
asmaa <asmaatareq1999@gmail.com>
2024-01-17 18:39:16 +0200

    implement page only for admin to remove users

--- lib/screen/remove_user.dart
+++ lib/screen/remove_user.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:spectrum_speak/constant/const_color.dart';
+import 'package:spectrum_speak/rest/rest_api_admin.dart';
+import 'package:spectrum_speak/units/build_search_text_field.dart';
+import 'package:spectrum_speak/widgets/card_user.dart';
 import 'package:spectrum_speak/widgets/top_bar.dart';
 
 class RemoveUser extends StatefulWidget {
@@ -27,8 +31,72 @@ class RemoveUserCall extends StatefulWidget {
 }
 
 class _RemoveUserCallState extends State<RemoveUserCall> {
+  final TextEditingController _searchController = TextEditingController();
+  List<dynamic> usersData = [];
+
+  Future<void> getUsers(String namePrefix) async {
+    try {
+      var data = await searchToUsers(namePrefix);
+
+      setState(() {
+        usersData = data;
+      });
+    } catch (error) {
+      print('Error fetching users data: $error');
+    }
+  }
+  @override
+  void initState() {
+    super.initState();
+    getUsers('');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CustomSearchTextField(
+                labelText: 'Search',
+                placeholder: 'Search',
+                controller: _searchController,
+                onTextChanged: (text) {
+                  getUsers(text);
+                },
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              if (usersData.isNotEmpty)
+                Column(
+                  children: usersData.map((data) {
+                    return CardUser(
+                      userID: data['UserID'].toString(),
+                      userName: data['Username'] ?? '',
+                      userCategory: data['Category'] ?? '',
+                    );
+                  }).toList(),
+                )
+              else
+                Center(
+                  child: Text(
+                    "No User Found",
+                    style: TextStyle(
+                      color: kDarkerColor.withOpacity(0.7),
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/widgets/card_user.dart
@@ -0,0 +1,210 @@
+import 'package:circular_profile_avatar/circular_profile_avatar.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:mailer/smtp_server/gmail.dart';
+import 'package:spectrum_speak/constant/const_color.dart';
+import 'package:spectrum_speak/rest/rest_api_menu.dart';
+import 'package:spectrum_speak/rest/rest_api_profile_delete.dart';
+import 'package:spectrum_speak/screen/parent_profile.dart';
+import 'package:spectrum_speak/screen/shadow_teacher_profile.dart';
+import 'package:spectrum_speak/screen/specialist_profile.dart';
+import 'package:mailer/mailer.dart';
+
+class CardUser extends StatefulWidget {
+  final String userID;
+  final String userName;
+  final String userCategory;
+  const CardUser({
+    super.key,
+    required this.userID,
+    required this.userName,
+    required this.userCategory,
+  });
+
+  @override
+  State<CardUser> createState() => _CardUserState();
+}
+
+class _CardUserState extends State<CardUser> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 3,
+      shadowColor: kDarkerColor,
+      color: kPrimary,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: InkWell(
+        onTap: () {
+          switch (widget.userCategory) {
+            case "Parent":
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ParentProfile(
+                    userID: widget.userID,
+                  ),
+                ),
+              );
+              break;
+            case "Specialist":
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => SpecialistProfile(
+                          userId: widget.userID,
+                        )),
+              );
+              break;
+            case "ShadowTeacher":
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ShadowTeacherProfile(
+                    userId: widget.userID,
+                  ),
+                ),
+              );
+              break;
+            default:
+              print("error in category");
+              break;
+          }
+        },
+        child: ListTile(
+          leading: CircularProfileAvatar(
+            '',
+            borderWidth: 1.0,
+            borderColor: kDarkerColor,
+            backgroundColor: kPrimary,
+            radius: 25.0,
+            child: Image.asset(
+              'images/prof.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+          title: Text(
+            widget.userName,
+            style: TextStyle(
+              color: kDarkerColor,
+            ),
+          ),
+          subtitle: Text(
+            widget.userCategory,
+            maxLines: 1,
+          ),
+          trailing: ElevatedButton.icon(
+            onPressed: () async {
+              bool confirmed = await _showDestroyAccountConfirmation(context);
+              String email = await getEmailByID(widget.userID);
+              if (confirmed) {
+                switch (widget.userCategory) {
+                  case "Parent":
+                    deleteParent(widget.userID);
+                    sendEmail(email, widget.userName);
+                    break;
+                  case "Specialist":
+                    deleteSpecialist(widget.userID);
+                    sendEmail(email, widget.userName);
+                    break;
+                  case "ShadowTeacher":
+                    deleteShadowTeacher(widget.userID);
+                    sendEmail(email, widget.userName);
+                    break;
+                  default:
+                    print("error in category");
+                    return;
+                }
+              }
+            },
+            style: ElevatedButton.styleFrom(
+              foregroundColor: kPrimary,
+              backgroundColor: kBlue,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(
+                  7,
+                ),
+              ),
+            ),
+            icon: Icon(
+              FontAwesomeIcons.userMinus,
+              color: kPrimary,
+              size: 17,
+            ),
+            label: Text(
+              "Remove",
+              style: TextStyle(
+                color: kPrimary,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<bool> _showDestroyAccountConfirmation(BuildContext context) async {
+    return await showDialog<bool>(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text("Warning"),
+              content: Text("Are you sure you want to destroy this account?"
+                  "This action cannot be undone."),
+              icon: Icon(
+                Icons.warning_amber,
+                size: 45,
+                color: kYellow,
+              ),
+              actions: <Widget>[
+                TextButton(
+                  child: Text("Cancel"),
+                  onPressed: () {
+                    Navigator.of(context)
+                        .pop(false); // Close the dialog and return false
+                  },
+                ),
+                TextButton(
+                  child: Text("Yes"),
+                  onPressed: () {
+                    Navigator.of(context)
+                        .pop(true); // Close the dialog and return true
+                  },
+                ),
+              ],
+            );
+          },
+        ) ??
+        false; // Return false if the user dismisses the dialog without making a choice
+  }
+
+  Future<void> sendEmail(String userEmail, String userName) async {
+    // Configure your email server and credentials
+    final smtpServer = gmail('asmaatareq1999@Gmail.com', 'evqbiqbqqezaefvl');
+
+    // Create a message
+    final message = Message()
+      ..from = Address('asmaatareq1999@gmail.com', 'Spectrum Speak Application')
+      ..recipients.add('$userEmail')
+      ..subject = 'Account Deleting'
+      ..text = 'Dear $userName \n\n'
+          'I hope this email finds you well. We regret to inform you that your account on the Spectrum Speak App is scheduled for deletion. After a thorough review, we have observed frequent complaints against your account or instances of misuse of the app.\n\n'
+          'If you believe this decision is in error or would like to discuss further, please contact our support team at [SpectrumSpeak@gmail.com] within the next 5 days.\n\n'
+          'Thank you for your understanding.\n\nBest regards,\n\nSpectrum Speak App Team';
+    try {
+      // Send the email
+      final sendReport = await send(message, smtpServer);
+
+      print('Message sent: ' + sendReport.toString());
+    } catch (e) {
+      print('Error occurred: $e');
+    }
+  }
+}
