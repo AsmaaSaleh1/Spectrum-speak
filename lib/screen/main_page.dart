0f8d1efecc6b20806fd7bf576932c731a019ccf0
asmaa <asmaatareq1999@gmail.com>
2024-01-26 17:22:58 +0200

    start in Main page design

--- lib/screen/main_page.dart
+++ lib/screen/main_page.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:spectrum_speak/constant/const_color.dart';
 import 'package:spectrum_speak/rest/auth_manager.dart';
+import 'package:spectrum_speak/units/custom_button.dart';
+import 'package:spectrum_speak/units/custom_clipper.dart';
+import 'package:spectrum_speak/units/custom_clipper_Main_square.dart';
+import 'package:spectrum_speak/units/custom_clipper_main.dart';
+import 'package:spectrum_speak/units/custom_clipper_main_upper.dart';
+import 'package:spectrum_speak/units/custom_clipper_puzzle.dart';
+import 'package:spectrum_speak/units/custom_main_button.dart';
 import 'package:spectrum_speak/widgets/top_bar.dart';
 
 import 'login.dart';
@@ -12,7 +21,6 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-
   @override
   void initState() {
     super.initState();
@@ -31,17 +39,174 @@ class _MainPageState extends State<MainPage> {
       );
     }
   }
+
   @override
-  Widget build(BuildContext context) {
-    return TopBar(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text("MAIN"),
-          SizedBox(height: 10),
-        ],
-      ),
-    );
-  }
+  Widget build(BuildContext context) => LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          double screenWidth = constraints.maxWidth;
+          double linePadding;
+          if (screenWidth >= 1200) {
+            linePadding = 110;
+          } else if (screenWidth >= 800) {
+            linePadding = 70;
+          } else {
+            linePadding = 20;
+          }
+          return TopBar(
+            body: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Stack(
+                    children: [
+                      Container(
+                        child: ClipPath(
+                          clipper: MyCustomClipperMainSquare(),
+                          child: Container(
+                            color: kYellow,
+                            height: screenWidth,
+                            width: screenWidth,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        alignment: Alignment.topCenter,
+                        height: screenWidth,
+                        width: screenWidth,
+                        child: ClipPath(
+                          clipper: MyCustomClipperMainUpper(),
+                          child: Container(
+                            height: 300.0,
+                            color: kGreen,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        alignment: Alignment.topCenter,
+                        height: screenWidth,
+                        width: screenWidth,
+                        child: ClipPath(
+                          clipper: MyCustomClipperMain(),
+                          child: Container(
+                            height: 300.0,
+                            color: kBlue,
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        bottom: screenWidth / 2,
+                        left: linePadding,
+                        right: 0,
+                        child: Row(
+                          children: [
+                            Container(
+                              child: Text(
+                                'Welcome Back,\n Asmaa!!',
+                                style: TextStyle(
+                                  color: kDarkerColor,
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 30),
+                              child: Container(
+                                height: screenWidth / 3,
+                                child: Image.asset(
+                                  'images/spectrumSpeakWithoutWard.png',
+                                  width: screenWidth / 3,
+                                  height: screenWidth / 3,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Positioned(
+                        bottom: screenWidth / 40,
+                        left: (screenWidth-(linePadding*2)-(120*3))/2 ,
+                        child: SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Row(
+                            children: [
+                              Container(
+                                width: 120,
+                                height: 120,
+                                child: CustomMainButton(
+                                  foregroundColor: kPrimary,
+                                  backgroundColor: kBlue,
+                                  onPressed: () {},
+                                  buttonText: 'To-DO',
+                                  icon: const Icon(
+                                    FontAwesomeIcons.clipboardList,
+                                    size: 35.0,
+                                  ),
+                                  iconColor: kPrimary,
+                                ),
+                              ),
+                              SizedBox(width: linePadding,),
+                              Container(
+                                width: 120,
+                                height: 120,
+                                child: CustomMainButton(
+                                  foregroundColor: kPrimary,
+                                  backgroundColor: kBlue,
+                                  onPressed: () {},
+                                  buttonText: 'Spectrum\nBot',
+                                  icon: const Icon(
+                                    FontAwesomeIcons.brain,
+                                    size: 35.0,
+                                  ),
+                                  iconColor: kPrimary,
+                                ),
+                              ),
+                              SizedBox(width: linePadding,),
+                              Container(
+                                width: 120,
+                                height: 120,
+                                child: CustomMainButton(
+                                  foregroundColor: kPrimary,
+                                  backgroundColor: kBlue,
+                                  onPressed: () {},
+                                  buttonText: 'To-DO',
+                                  icon: const Icon(
+                                    Icons.edit,
+                                    size: 35.0,
+                                  ),
+                                  iconColor: kPrimary,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10,),
+                  Row(
+                    children: [
+                      SizedBox(width: linePadding,),
+                      Icon(
+                        FontAwesomeIcons.businessTime,
+                        size: 28,
+                        color: kDarkerColor,
+                      ),
+                      SizedBox(width: linePadding,),
+                      Text(
+                        "Week Centers Events",
+                        style: TextStyle(
+                          color: kDarkerColor,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      );
 }
---
+++ lib/units/custom_main_button.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+import 'package:spectrum_speak/constant/const_color.dart';
+
+class CustomMainButton extends StatelessWidget {
+  final Color foregroundColor;
+  final Color backgroundColor;
+  final VoidCallback onPressed;
+  final String buttonText;
+  final Icon icon;
+  final Color iconColor;
+
+  const CustomMainButton({
+    super.key,
+    required this.foregroundColor,
+    required this.backgroundColor,
+    required this.onPressed,
+    required this.buttonText,
+    required this.icon,
+    required this.iconColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        foregroundColor: foregroundColor,
+        backgroundColor: backgroundColor,
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        elevation: 0,
+        shadowColor: Colors.transparent,
+      ).copyWith(
+        shadowColor: MaterialStateProperty.resolveWith((states) {
+          return kDarkBlue;
+        }),
+        elevation: MaterialStateProperty.resolveWith((states) {
+          if (states.contains(MaterialState.pressed)) {
+            return 15;
+          }
+          return 5;
+        }),
+      ),
+      child: IntrinsicWidth(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              icon.icon,
+              size: icon.size,
+              color: iconColor,
+            ),
+            const SizedBox(height: 5),
+            Flexible(
+              child: Text(
+                buttonText,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
