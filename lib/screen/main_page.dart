9fa085f50691f977fbadd98c26222c20ffd4e714
asmaa <asmaatareq1999@gmail.com>
2024-01-28 02:55:08 +0200

    add donation in main page

--- lib/screen/main_page.dart
+++ lib/screen/main_page.dart
@@ -29,7 +29,7 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   String category = "";
   String userName = "";
-  String city="";
+  String city = "";
   List<Booking> bookingList = [];
   List<Event> eventList = [];
   Future<List<Widget>?> getBookingWidgets() async {
@@ -42,16 +42,18 @@ class _MainPageState extends State<MainPage> {
         )
         .toList();
   }
+
   Future<List<Widget>?> getEventsWidgets() async {
     return eventList
         .map(
           (event) => CardEvent(
-        events: event,
-        category: category,
-      ),
-    )
+            events: event,
+            category: category,
+          ),
+        )
         .toList();
   }
+
   Future getData() async {
     try {
       String? userId = await AuthManager.getUserId();
@@ -59,18 +61,18 @@ class _MainPageState extends State<MainPage> {
       if (userId != null) {
         String cat = await getUserCategory(userId);
         String user = await getUserName(userId);
-        String cit= await getCity(userId);
+        String cit = await getCity(userId);
         setState(() {
           userName = user;
           category = cat;
-          city=cit;
+          city = cit;
         });
         if (category != '') {
           var listBook = await getBookingsForSevenDay(userId, category);
-          var listEvent= await getEventsForSevenDay("Jerusalem");
+          var listEvent = await getEventsForSevenDay("Jerusalem");
           setState(() {
             bookingList = listBook;
-            eventList=listEvent;
+            eventList = listEvent;
           });
         } else {
           print("Error: Empty category in getData class main page");
@@ -235,7 +237,7 @@ class _MainPageState extends State<MainPage> {
                                 child: CustomMainButton(
                                   foregroundColor: kPrimary,
                                   backgroundColor: kBlue,
-                                  onPressed: ()=> Navigator.push(
+                                  onPressed: () => Navigator.push(
                                     context,
                                     MaterialPageRoute(
                                       builder: (context) => const Search(),
@@ -258,79 +260,82 @@ class _MainPageState extends State<MainPage> {
                   SizedBox(
                     height: 10,
                   ),
-                  Row(
-                    children: [
-                      SizedBox(
-                        width: linePadding,
-                      ),
-                      Icon(
-                        FontAwesomeIcons.bookBookmark,
-                        size: 28,
-                        color: kDarkerColor,
-                      ),
-                      SizedBox(
-                        width: linePadding,
-                      ),
-                      Text(
-                        "Booking for This Week",
-                        style: TextStyle(
-                          color: kDarkerColor,
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
+                  if (category != "ShadowTeacher")
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: linePadding,
                         ),
-                      ),
-                    ],
-                  ),
-                  FutureBuilder<List<Widget>?>(
-                    future: getBookingWidgets(),
-                    builder: (context, snapshot) {
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return Container(
-                          color: kPrimary,
-                          child: Container(
-                            alignment: Alignment.center,
-                            child: const CircularProgressIndicator(
-                              backgroundColor: kDarkBlue,
-                              color: kDarkBlue,
-                            ),
+                        Icon(
+                          FontAwesomeIcons.bookBookmark,
+                          size: 28,
+                          color: kDarkerColor,
+                        ),
+                        SizedBox(
+                          width: linePadding,
+                        ),
+                        Text(
+                          "Booking for This Week",
+                          style: TextStyle(
+                            color: kDarkerColor,
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
                           ),
-                        );
-                      } else if (snapshot.hasError) {
-                        return Text('Error: ${snapshot.error}');
-                      } else {
-                        return Stack(
-                          children: [
-                            Visibility(
-                              visible: snapshot.data != null &&
-                                  snapshot.data!.isNotEmpty,
-                              child: SingleChildScrollView(
-                                scrollDirection: Axis.horizontal,
-                                child: Row(
-                                  children: snapshot.data ??
-                                      [], // Provide a default value when null
-                                ),
+                        ),
+                      ],
+                    ),
+                  if (category != "ShadowTeacher")
+                    FutureBuilder<List<Widget>?>(
+                      future: getBookingWidgets(),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return Container(
+                            color: kPrimary,
+                            child: Container(
+                              alignment: Alignment.center,
+                              child: const CircularProgressIndicator(
+                                backgroundColor: kDarkBlue,
+                                color: kDarkBlue,
                               ),
                             ),
-                            Visibility(
-                              visible: snapshot.data == null &&
-                                  snapshot.data!.isEmpty,
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Text(
-                                  "No Booking For This Week",
-                                  style: TextStyle(
-                                    fontSize: 17.0,
-                                    fontWeight: FontWeight.bold,
-                                    color: kBlue,
+                          );
+                        } else if (snapshot.hasError) {
+                          return Text('Error: ${snapshot.error}');
+                        } else {
+                          return Stack(
+                            children: [
+                              Visibility(
+                                visible: snapshot.data != null ||
+                                    snapshot.data!.isNotEmpty,
+                                child: SingleChildScrollView(
+                                  scrollDirection: Axis.horizontal,
+                                  child: Row(
+                                    children: snapshot.data ??
+                                        [], // Provide a default value when null
                                   ),
                                 ),
                               ),
-                            ),
-                          ],
-                        );
-                      }
-                    },
-                  ),
+                              Visibility(
+                                visible: snapshot.data == null ||
+                                    snapshot.data!.isEmpty,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    "No Booking For This Week",
+                                    style: TextStyle(
+                                      fontSize: 17.0,
+                                      fontWeight: FontWeight.bold,
+                                      color: kBlue,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          );
+                        }
+                      },
+                    ),
                   Row(
                     children: [
                       SizedBox(
@@ -404,7 +409,142 @@ class _MainPageState extends State<MainPage> {
                       }
                     },
                   ),
-                  SizedBox(height: 50,)
+                  Divider(
+                    color: kDarkBlue,
+                    thickness: 2.0,
+                    indent: linePadding,
+                    endIndent: linePadding,
+                  ),
+                  Container(
+                    alignment: Alignment.topLeft,
+                    width: 350,
+                    child: Text(
+                      "Be The Reason",
+                      style: TextStyle(
+                        fontSize: 35.0,
+                        fontWeight: FontWeight.bold,
+                        color: kYellow.withGreen(180),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.topLeft,
+                    width: 350,
+                    child: Text(
+                      "Someone Smiles Today!",
+                      style: TextStyle(
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.bold,
+                        color: kDarkBlue,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.topLeft,
+                    width: 350,
+                    child: Text(
+                      "Join us in creating brighter futures and fostering a world where every child on the autism spectrum receives the support they deserve. Your support makes a meaningful difference.",
+                      style: TextStyle(
+                        fontSize: 15.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey.shade700,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.topLeft,
+                    width: 350,
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 10.0),
+                      child: Text(
+                        "For further details, please don't hesitate to reach out to us through the following channels:",
+                        style: TextStyle(
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.bold,
+                          color: kDarkerColor,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 350,
+                    alignment: Alignment.topLeft,
+                    margin: const EdgeInsets.only(left: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(height: 10),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.email,
+                              size: 30,
+                              color: kDarkBlue,
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Email:',
+                                  style: TextStyle(
+                                    color: kDarkerColor,
+                                    fontSize: 17,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  'SpectrumSpeak@gmail.com',
+                                  style: TextStyle(
+                                    color: kDarkerColor,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.phone,
+                              size: 30,
+                              color: kDarkBlue,
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Phone:',
+                                  style: TextStyle(
+                                    color: kDarkerColor,
+                                    fontSize: 17,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                Text(
+                                  '+(970) 592-676-710',
+                                  style: TextStyle(
+                                    color: kDarkerColor,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 50,
+                  )
                 ],
               ),
             ),
